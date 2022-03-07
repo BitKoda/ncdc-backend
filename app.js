3fb790c9af2101e8ecebae99ecb8fc1dae9a92b8
Jamie Finlay <jpfinlay@gmail.com>
2022-03-07 10:49:18 +0000

    Move initializing of cors to _after_ line where 'app' is defined

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 
 const cors = require('cors');
-app.use(cors());
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const { 
   handlePSQLerr, 
